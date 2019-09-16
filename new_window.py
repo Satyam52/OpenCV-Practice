2a6b49dd97f67dc4b1cb9127eea0490de4740dcb
Abhishek Kumar Gupta <45595749+Satyam52@users.noreply.github.com>
2019-09-17 01:04:08 +0530

    Add files via upload

---
+++ new_window.py
@@ -0,0 +1,11 @@
+import cv2
+
+img = cv2.imread('Colored_flip.jpg')
+
+while True:
+    cv2.imshow("Puppy", img)
+
+    if cv2.waitKey(1) & 0xFF == 27:
+        break
+        
+cv2.destroyAllWindows()
